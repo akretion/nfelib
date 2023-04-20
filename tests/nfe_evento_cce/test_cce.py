bee86fdd34c3810d850d28cc879a6e5e5df20689
Raphaël Valyi <rvalyi@akretion.com>
2023-04-20 01:59:08 -0300

    added xsdata cce in/out test

---
+++ tests/nfe_evento_cce/test_cce.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2019 - TODAY Raphaël Valyi - Akretion
+
+import os
+from xmldiff import main
+
+from xsdata.formats.dataclass.serializers.config import SerializerConfig
+from xsdata.formats.dataclass.parsers import XmlParser
+from xsdata.formats.dataclass.serializers import XmlSerializer
+from pathlib import Path
+
+from nfelib.bindings.nfe_evento_cce.v1_0.cce_v1_00 import Evento
+
+
+def test_in_out_leiauteCCe():
+    path = os.path.join("nfelib", "samples", "nfe_evento_cce", "v1_0")
+    for filename in os.listdir(path):
+        input_file = os.path.join(path, filename)
+        parser = XmlParser()
+        obj = parser.from_path(Path(input_file), Evento)
+        serializer = XmlSerializer(config=SerializerConfig(pretty_print=True))
+        xml = serializer.render(
+            obj=obj, ns_map={None: "http://www.portalfiscal.inf.br/nfe"}
+        )
+
+        output_file = "tests/output_nfe_evento_cce.xml"
+        with open(output_file, "w") as f:
+            f.write(xml)
+
+        diff = main.diff_files(input_file, output_file)
+        assert len(diff) == 0
+        if len(diff) != 0:
+            break

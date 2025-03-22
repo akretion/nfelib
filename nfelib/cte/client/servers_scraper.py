ab975fa01d099980fa5705b56ce09452f71c65ba
Raphaël Valyi <rvalyi@akretion.com>
2025-03-22 15:41:40 +0000

    add CT-e scraper/servers

---
+++ nfelib/cte/client/servers_scraper.py
@@ -0,0 +1,25 @@
+import logging
+from pathlib import Path
+from typing import Dict, Any
+from io import StringIO  # Add this import
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from xsdata.formats.dataclass.serializers import PycodeSerializer
+from nfelib.utils.servers_scraper import fetch_servers, save_servers
+
+# Constants
+PROD_URL = "https://www.cte.fazenda.gov.br/portal/webServices.aspx"
+DEV_URL = "https://hom.cte.fazenda.gov.br/portal/webServices.aspx"
+OUTPUT_FILE = Path("nfelib/cte/client/servers.py")
+
+
+def main():
+    servers, constants = fetch_servers(PROD_URL, DEV_URL)
+    if servers:
+        save_servers(servers, constants, OUTPUT_FILE)
+
+
+if __name__ == "__main__":
+    main()

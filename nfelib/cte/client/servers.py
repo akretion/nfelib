f986ba9a49400e8216162fc2757c9193080cdd6e
Raphaël Valyi <rvalyi@akretion.com>
2025-03-22 15:41:40 +0000

    add CT-e scraper/servers

---
+++ nfelib/cte/client/servers.py
@@ -0,0 +1,109 @@
+# Auto-generated file. Do not edit manually.
+
+# Constants
+CTESTATUSSERVICOV4 = "CteStatusServicoV4"
+CTECONSULTAV4 = "CteConsultaV4"
+CTERECEPCAOEVENTOV4 = "CteRecepcaoEventoV4"
+CTERECEPCAOOSV4 = "CTeRecepcaoOSV4"
+CTERECEPCAOSINCV4 = "CTeRecepcaoSincV4"
+CTERECEPCAOGTVEV4 = "CTeRecepcaoGTVeV4"
+CTERECEPCAOSIMPV4 = "CTeRecepcaoSimpV4"
+
+# Servers
+
+
+servers = {
+    'MT': {
+        'prod_server': 'cte.sefaz.mt.gov.br',
+        'dev_server': 'homologacao.sefaz.mt.gov.br',
+        'CteStatusServicoV4': '/ctews2/services/CTeStatusServicoV4?wsdl',
+        'CteConsultaV4': '/ctews2/services/CTeConsultaV4?wsdl',
+        'CteRecepcaoEventoV4': '/ctews2/services/CTeRecepcaoEventoV4?wsdl',
+        'CTeRecepcaoOSV4': '/ctews/services/CTeRecepcaoOSV4?wsdl',
+        'CTeRecepcaoSincV4': '/ctews2/services/CTeRecepcaoSincV4?wsdl',
+        'CTeRecepcaoGTVeV4': '/ctews2/services/CTeRecepcaoGTVeV4?wsdl',
+        'CTeRecepcaoSimpV4': '/cte-ws/services/CTeRecepcaoSimpV4',
+    },
+    'MS': {
+        'prod_server': 'producao.cte.ms.gov.br',
+        'dev_server': 'homologacao.cte.ms.gov.br',
+        'CteStatusServicoV4': '/ws/CTeStatusServicoV4',
+        'CteConsultaV4': '/ws/CTeConsultaV4',
+        'CteRecepcaoEventoV4': '/ws/CTeRecepcaoEventoV4',
+        'CTeRecepcaoOSV4': '/ws/CTeRecepcaoOSV4',
+        'CTeRecepcaoSincV4': '/ws/CTeRecepcaoSincV4',
+        'CTeRecepcaoGTVeV4': '/ws/CTeRecepcaoGTVeV4',
+        'CTeRecepcaoSimpV4': '/ws/CTeRecepcaoSimpV4',
+    },
+    'MG': {
+        'prod_server': 'cte.fazenda.mg.gov.br',
+        'dev_server': 'hcte.fazenda.mg.gov.br',
+        'CteStatusServicoV4': '/cte/services/CTeStatusServicoV4',
+        'CteConsultaV4': '/cte/services/CTeConsultaV4',
+        'CteRecepcaoEventoV4': '/cte/services/CTeRecepcaoEventoV4',
+        'CTeRecepcaoOSV4': '/cte/services/CTeRecepcaoOSV4',
+        'CTeRecepcaoSincV4': '/cte/services/CTeRecepcaoSincV4',
+        'CTeRecepcaoGTVeV4': '/cte/services/CTeRecepcaoGTVeV4',
+        'CTeRecepcaoSimpV4': '/cte/services/CTeRecepcaoSimpV4',
+    },
+    'PR': {
+        'prod_server': 'cte.fazenda.pr.gov.br',
+        'dev_server': 'homologacao.cte.fazenda.pr.gov.br',
+        'CteStatusServicoV4': '/cte4/CTeStatusServicoV4?wsdl',
+        'CteConsultaV4': '/cte4/CTeConsultaV4?wsdl',
+        'CteRecepcaoEventoV4': '/cte4/CTeRecepcaoEventoV4?wsdl',
+        'CTeRecepcaoOSV4': '/cte4/CTeRecepcaoOSV4?wsdl',
+        'CTeRecepcaoSincV4': '/cte4/CTeRecepcaoSincV4?wsdl',
+        'CTeRecepcaoGTVeV4': '/cte4/CTeRecepcaoGTVeV4?wsdl',
+        'CTeRecepcaoSimpV4': '/cte4/CTeRecepcaoSimpV4',
+    },
+    'RS': {
+        'prod_server': 'cte.svrs.rs.gov.br',
+        'dev_server': 'cte-homologacao.svrs.rs.gov.br',
+        'CteStatusServicoV4': '/ws/CTeStatusServicoV4/CTeStatusServicoV4.asmx',
+        'CteConsultaV4': '/ws/CTeConsultaV4/CTeConsultaV4.asmx',
+        'CteRecepcaoEventoV4': '/ws/CTeRecepcaoEventoV4/CTeRecepcaoEventoV4.asmx',
+        'CTeRecepcaoOSV4': '/ws/CTeRecepcaoOSV4/CTeRecepcaoOSV4.asmx',
+        'CTeRecepcaoSincV4': '/ws/CTeRecepcaoSincV4/CTeRecepcaoSincV4.asmx',
+        'CTeRecepcaoGTVeV4': '/ws/CTeRecepcaoGTVeV4/CTeRecepcaoGTVeV4.asmx',
+        'CTeRecepcaoSimpV4': '/ws/CTeRecepcaoSimpV4/CTeRecepcaoSimpV4.asmx',
+    },
+    'SP': {
+        'prod_server': 'nfe.fazenda.sp.gov.br',
+        'dev_server': 'homologacao.nfe.fazenda.sp.gov.br',
+        'CteStatusServicoV4': '/CTeWS/WS/CTeStatusServicoV4.asmx',
+        'CteConsultaV4': '/CTeWS/WS/CTeConsultaV4.asmx',
+        'CteRecepcaoEventoV4': '/CTeWS/WS/CTeRecepcaoEventoV4.asmx',
+        'CTeRecepcaoOSV4': '/CTeWS/WS/CTeRecepcaoOSV4.asmx',
+        'CTeRecepcaoSincV4': '/CTeWS/WS/CTeRecepcaoSincV4.asmx',
+        'CTeRecepcaoGTVeV4': '/CTeWS/WS/CTeRecepcaoGTVeV4.asmx',
+        'CTeRecepcaoSimpV4': '/CTeWS/WS/CTeRecepcaoSimpV4.asmx',
+    },
+    'SVRS': {
+        'prod_server': 'cte.svrs.rs.gov.br',
+        'dev_server': 'cte-homologacao.svrs.rs.gov.br',
+        'CteStatusServicoV4': '/ws/CTeStatusServicoV4/CTeStatusServicoV4.asmx',
+        'CteConsultaV4': '/ws/CTeConsultaV4/CTeConsultaV4.asmx',
+        'CteRecepcaoEventoV4': '/ws/CTeRecepcaoEventoV4/CTeRecepcaoEventoV4.asmx',
+        'CTeRecepcaoOSV4': '/ws/CTeRecepcaoOSV4/CTeRecepcaoOSV4.asmx',
+        'CTeRecepcaoSincV4': '/ws/CTeRecepcaoSincV4/CTeRecepcaoSincV4.asmx',
+        'CTeRecepcaoGTVeV4': '/ws/CTeRecepcaoGTVeV4/CTeRecepcaoGTVeV4.asmx',
+        'CTeRecepcaoSimpV4': '/ws/CTeRecepcaoSimpV4/CTeRecepcaoSimpV4.asmx',
+    },
+    'SVSP': {
+        'prod_server': 'nfe.fazenda.sp.gov.br',
+        'dev_server': 'homologacao.nfe.fazenda.sp.gov.br',
+        'CteStatusServicoV4': '/CTeWS/WS/CTeStatusServicoV4.asmx',
+        'CteConsultaV4': '/CTeWS/WS/CTeConsultaV4.asmx',
+        'CteRecepcaoEventoV4': '/CTeWS/WS/CTeRecepcaoEventoV4.asmx',
+        'CTeRecepcaoOSV4': '/CTeWS/WS/CTeRecepcaoOSV4.asmx',
+        'CTeRecepcaoSincV4': '/CTeWS/WS/CTeRecepcaoSincV4.asmx',
+        'CTeRecepcaoGTVeV4': '/CTeWS/WS/CTeRecepcaoGTVeV4.asmx',
+        'CTeRecepcaoSimpV4': '/CTeWS/WS/CTeRecepcaoSimpV4.asmx',
+    },
+    'AN': {
+        'prod_server': 'www1.cte.fazenda.gov.br',
+        'dev_server': 'hom1.cte.fazenda.gov.br',
+    },
+}
+

53060710619229454d704be30f55afd424344a04
Raphaël Valyi <rvalyi@akretion.com>
2026-09-25 18:44:33 -0300

    feat(cte): update schemas to PL_CTe_400_NT2026.002_RTC_v1.00 and regenerate bindings
    Update the CT-e schemas from the Fazenda package "NT 2026.002_RTC_v.1.00",
    published 16/06/2026.
    
    Source: https://www.cte.fazenda.gov.br/portal/exibirArquivo.aspx?conteudo=EGfyMjCQNeE=
    
    Changed: cteTiposBasico_v4.00.xsd, DFeTiposBasicos_v1.00.xsd.
    Added back: evVincPgto_v4.00.xsd, evCancVincPgto_v4.00.xsd (this package ships
    the 46-file set with the VincPgto events of the 2026.001/2026.002 track).
    
    Regenerate the bindings with xsdata 25.7.

--- nfelib/cte/bindings/v4_0/ev_canc_vinc_pgto_v4_00.py
+++ nfelib/cte/bindings/v4_0/ev_canc_vinc_pgto_v4_00.py
@@ -13,31 +13,33 @@ from typing import Optional
 __NAMESPACE__ = "http://www.portalfiscal.inf.br/cte"
 
 
-class EvCancIecteDescEvento(Enum):
-    CANCELAMENTO_DO_INSUCESSO_DE_ENTREGA_DO_CT_E = (
-        "Cancelamento do Insucesso de Entrega do CT-e"
+class EvCancVincPgtoDescEvento(Enum):
+    CANCELAMENTO_DA_VINCULA_O_DO_PAGAMENTO = (
+        "Cancelamento da vinculação do pagamento"
+    )
+    CANCELAMENTO_DA_VINCULACAO_DO_PAGAMENTO = (
+        "Cancelamento da vinculacao do pagamento"
     )
 
 
 @dataclass
-class EvCancIecte:
+class EvCancVincPgto:
     """
-    Schema XML de validação do evento cancelamento do insucesso de entrega
-    eletrônico do CT-e 110191.
+    Schema XML de validação do evento cancelamento da vinculação do pgto 110301.
     "
 
-    :ivar descEvento: Descrição do Evento - “Cancelamento do Insucesso
-        de Entrega do CT-e”
-    :ivar nProt: Número do Protocolo de autorização do CT-e
-    :ivar nProtIE: Número do Protocolo de autorização do evento a ser
-        cancelado
+    :ivar descEvento: Descrição do Evento - “Cancelamento da vinculação
+        do pagamento”
+    :ivar nProt: Número do Protocolo de autorização do DFe
+    :ivar nProtVincPgto: Número do Protocolo de autorização do evento a
+        ser cancelado
     """
 
     class Meta:
-        name = "evCancIECTe"
+        name = "evCancVincPgto"
         namespace = "http://www.portalfiscal.inf.br/cte"
 
-    descEvento: Optional[EvCancIecteDescEvento] = field(
+    descEvento: Optional[EvCancVincPgtoDescEvento] = field(
         default=None,
         metadata={
             "type": "Element",
@@ -54,7 +56,7 @@ class EvCancIecte:
             "pattern": r"[0-9]{15}",
         },
     )
-    nProtIE: Optional[str] = field(
+    nProtVincPgto: Optional[str] = field(
         default=None,
         metadata={
             "type": "Element",
